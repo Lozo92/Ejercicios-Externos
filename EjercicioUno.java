6bd476d9da9275e9b086c236836f36d791b3dc84
JavaBasico-Ejercicios <https://github.com/Lozo92/JavaBasico-Ejercicios.git>
2021-12-15 12:33:29 +0100

    Comienzo del proyecto de ejercicios externos

---
+++ EjercicioUno.java
@@ -0,0 +1,15 @@
+package com.jonathan;
+
+public class EjercicioUno {
+
+    public static void main(String[] args) {
+
+        //Escribir programa que imprima en pantalla mi nombre.
+
+        System.out.println("Jonathan Lozano Casanova");
+
+        //Modificación de programa para que, además, escriba la calle y el número de teléfono.
+        System.out.println("Calle General");
+        System.out.println("633888333");
+    }
+}
